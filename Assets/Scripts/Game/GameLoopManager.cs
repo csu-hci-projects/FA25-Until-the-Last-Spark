674159e58f7aa4db4a346fe8886631c62cc48123
cschwab <cschwab@colostate.edu>
2025-10-20 16:41:35 -0600

    added test map w/ nodes for enemies
    i made a little map with nodes on the path that you can make enemies follow. I also added a blank gamemaster that will control the game

---
+++ Assets/Scripts/Game/GameLoopManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoopManager : MonoBehaviour
+{
+
+    public bool endLoop;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    IEnumerator GameLoop() {
+        while(endLoop == false) {
+            //Spawn Enemies
+
+            // Spawn Towers
+
+            //Move Enemies
+
+            //Tick Towers
+
+            //Apply Effects
+
+            //Damage Enemies
+
+            //Remove Enemies
+
+            //Remove Towers
+
+            yield return null;
+        }
+    }
+}
