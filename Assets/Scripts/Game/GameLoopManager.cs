8293900b0fc6ba95d8ac7327e2bf4518234baaaa
bbenfiel <bbenfiel@colostate.edu>
2025-10-24 20:59:44 -0600

    Shop now works but there is no money system yet

--- Assets/Scripts/Game/GameLoopManager.cs
+++ Assets/Scripts/Game/GameLoopManager.cs
@@ -7,6 +7,7 @@ public class GameLoopManager : MonoBehaviour
 
     public bool endLoop;
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,7 +15,7 @@ public class GameLoopManager : MonoBehaviour
     }
 
     IEnumerator GameLoop() {
-        while(endLoop == false) {
+        while (endLoop == false) {
             //Spawn Enemies
 
             // Spawn Towers
--- Assets/Scripts/Game/ShopMenu.cs
+++ Assets/Scripts/Game/ShopMenu.cs
@@ -2,8 +2,18 @@ using UnityEngine;
 
 public class ShopMenu : MonoBehaviour
 {
+
     public static bool isShopOpen = false;
+    public static bool boughtTower = false;
+
+    public int towerType = 0; // 0 = towerPrefab0, 1 = towerPrefab1, 2 = towerPrefab2
     public GameObject shopMenuUI;
+
+    [SerializeField] int placeObjectCamerOffset = 30;
+    [SerializeField] GameObject[] towerPrefab;
+
+
+
 
     // Update is called once per frame
     void Update()
@@ -14,13 +24,37 @@ public class ShopMenu : MonoBehaviour
             if (isShopOpen)
             {
                 closeShop();
-                
+
             }
             else
             {
                 openShop();
             }
         }
+
+        if (boughtTower && Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("In buy mode, click to place tower.");
+           
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = placeObjectCamerOffset; // Set this to be the distance from the camera to the object you want to place
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            switch (towerType) //Can add more tower types here as needed
+            {
+                case 0: 
+                    Instantiate(towerPrefab[0], worldPos, Quaternion.identity);
+                    break;
+                case 1:
+                    Instantiate(towerPrefab[1], worldPos, Quaternion.identity);
+                    break;
+                default:
+                    Debug.LogWarning("Invalid tower type selected!");
+                    break;
+            }
+            boughtTower = false;   
+        }
+
     }
 
     void openShop()
@@ -36,4 +70,21 @@ public class ShopMenu : MonoBehaviour
         Time.timeScale = 1;
         isShopOpen = false;
     }
+
+    public void buyTower1()
+    {
+        Debug.Log("Tower Purchased!");
+        closeShop();
+        towerType = 0;
+        boughtTower = true;
+    }
+
+    public void buyTower2()
+    {
+        Debug.Log("Tower Purchased!");
+        closeShop();
+        towerType = 1;
+        boughtTower = true;
+    }
+
 }
