ef5121705a13f441cac7ffd1e4116f3bda018d0b
Jackson Stevenson <157153404+jakjak23@users.noreply.github.com>
2025-12-08 13:14:10 -0700

    Fixed a merge conflict error.
    Error deleted all of the wave system I had in place.

--- Assets/Scripts/Enemies/SpawnEnemy.cs
+++ Assets/Scripts/Enemies/SpawnEnemy.cs
@@ -1,48 +1,74 @@
 using UnityEngine;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
 
 public class SpawnEnemy : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] GameObject enemyPrefab;
-    [SerializeField] GameObject strongEnemyPrefab;
-    [SerializeField] GameObject heavyEnemyPrefab;
+    [SerializeField] GameObject[] enemyPrefab;
     [SerializeField] Transform pathContainer;
 
     [SerializeField] float spawnInterval = 2f;
     private int numOfEnemiesAlive = 0;
-    private int numOfEnemiesSpawned = 0;
 
     [SerializeField] int maxEnemies = 5;
 
+    private bool wave1Start = true;
+    private bool wave2Start = false;
+    private bool wave3Start = false;
+    private bool wave4Start = false;
+
     void Start()
     {
-        InvokeRepeating(nameof(Spawn), 0f, spawnInterval);
+        wave1Start = true;
+        wave2Start = false;
+        wave3Start = false;
+        wave4Start = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(GameLoopManager.wave == 1 && wave1Start)
+        {
+            wave1Start = false;
+            StartCoroutine(spawnWave1());
+            wave2Start = true;
+        }
+        
+        if(GameLoopManager.wave == 2 && wave2Start)
+        {
+            wave2Start = false;
+            StartCoroutine(spawnWave2());
+            wave3Start = true;
+        }
 
+        if(GameLoopManager.wave == 3 && wave3Start)
+        {
+            wave3Start = false;
+            StartCoroutine(spawnWave3());
+            wave4Start = true;
+        }
+
+        if(GameLoopManager.wave == 4 && wave4Start)
+        {
+            wave4Start = false;
+            StartCoroutine(spawnWave4());
+        }
+
+        if(GameLoopManager.wave == 4 && wave4Start == false && numOfEnemiesAlive == 0)
+        {
+            SceneManager.LoadScene(3); //Win Scene
+        }
     }
 
-    private void Spawn()
+    private void Spawn(int enemyPrefabNum)
     {
-        numOfEnemiesSpawned++;
-        if (numOfEnemiesSpawned <= maxEnemies)
+        if (numOfEnemiesAlive < maxEnemies)
         {
-              GameObject enemy;
-            if(numOfEnemiesSpawned == maxEnemies)
-            {
-                enemy = Instantiate(heavyEnemyPrefab, pathContainer.GetChild(0).position, Quaternion.identity);
-            }else if(numOfEnemiesSpawned % 10 != 0 )
-            {
-                 // Spawn the enemy at the first node
-                enemy = Instantiate(enemyPrefab, pathContainer.GetChild(0).position, Quaternion.identity);
-            }else
-            {
-                enemy = Instantiate(strongEnemyPrefab, pathContainer.GetChild(0).position, Quaternion.identity);
-            }
-           
+            // Spawn the enemy at the first node
+            GameObject enemy = Instantiate(enemyPrefab[enemyPrefabNum], pathContainer.GetChild(0).position, Quaternion.identity);
 
             // Assign all children of pathContainer as the path nodes
             Transform[] nodes = new Transform[pathContainer.childCount];
@@ -66,4 +92,97 @@ public class SpawnEnemy : MonoBehaviour
     {
         numOfEnemiesAlive--;
     }  
+
+    IEnumerator spawnWave1()
+    {
+        Debug.Log("Wave 1");
+        for (int i = 0; i < 15; i++)
+        {
+            Spawn(0);
+            yield return new WaitForSeconds(2f);
+        }
+
+        for (int i = 0; i < 15; i++)
+        {
+            Spawn(0);
+            yield return new WaitForSeconds(1f);
+        }
+        
+    }
+
+    IEnumerator spawnWave2()
+    {
+        Debug.Log("Wave 2");
+        for (int i = 0; i < 5; i++)
+        {
+            Spawn(0);
+            yield return new WaitForSeconds(2f);
+        }
+        for (int i = 0; i < 7; i++)
+        {
+            Spawn(1);
+            yield return new WaitForSeconds(.3f);
+            Spawn(0);
+            yield return new WaitForSeconds(1.5f);
+        }
+        for (int i = 0; i < 20; i++)
+        {
+            Spawn(0);
+            yield return new WaitForSeconds(.5f);
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            Spawn(1);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    IEnumerator spawnWave3()
+    {
+        Debug.Log("Wave 3");
+        for (int i = 0; i < 20; i++)
+        {
+            Spawn(2);
+            yield return new WaitForSeconds(.5f);
+        }
+        yield return new WaitForSeconds(5f);
+        for (int i = 0; i < 30; i++)
+        {
+            Spawn(2);
+            Spawn(0);
+            yield return new WaitForSeconds(.5f);
+        }
+        for (int i = 0; i < 20; i++)
+        {
+            Spawn(0);
+            yield return new WaitForSeconds(.3f);
+            Spawn(1);
+            yield return new WaitForSeconds(.3f);
+            Spawn(2);
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+
+    IEnumerator spawnWave4()
+    {
+        Debug.Log("Wave 4");
+        for (int i = 0; i < 5; i++)
+        {
+            Spawn(0);
+            yield return new WaitForSeconds(.3f);
+            Spawn(1);
+            yield return new WaitForSeconds(.5f);
+        }
+        Spawn(3);
+        yield return new WaitForSeconds(8f);
+        for (int i = 0; i < 30; i++)
+        {
+            Spawn(2);
+            yield return new WaitForSeconds(.1f);
+        }
+        Spawn(3);
+        yield return new WaitForSeconds(.8f);
+        Spawn(3);
+    }
+
 }
