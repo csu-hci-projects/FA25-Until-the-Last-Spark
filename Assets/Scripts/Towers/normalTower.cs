eb0b271fee83d85ada3abf2a4e931a901665393f
cschwab <cschwab@colostate.edu>
2025-12-09 22:54:13 -0700

    added textures to almost everything

--- Assets/Scripts/Towers/normalTower.cs
+++ Assets/Scripts/Towers/normalTower.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 public class normalTower : Tower
 {
     public GameObject arrowPrefab;
+    public GameObject gun;
     public Transform firePoint;
 
     protected override void Attack(GameObject enemy)
     {
         GameObject arrowGO = Instantiate(arrowPrefab, firePoint.position, firePoint.rotation);
         Arrow arrow = arrowGO.GetComponent<Arrow>();
+        Vector3 backupAngle = gun.transform.eulerAngles;
+        gun.transform.LookAt(enemy.transform);
+        gun.transform.eulerAngles = new Vector3(backupAngle.x, gun.transform.eulerAngles.y, gun.transform.eulerAngles.z);
 
         if (arrow != null)
         {
